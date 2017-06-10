6e93bbce555c2f80f608fad098c7d48dbeb77610
GODHANI\Godhani <ketan.godhani10@gmail.com>
2017-06-10 10:05:38 +0530

    commit #3

---
+++ AJSoftBAL/YarnTypeBL.cs
@@ -0,0 +1,47 @@
+﻿using AJSoftEntity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJSoftBAL
+{
+   public class YarnTypeBL
+    {
+        #region Get Methods
+
+        public YarnType GetById(int YarnTypeId)
+        {
+            try
+            {
+                using (var ctx = new DBAJEntities())
+                {
+                    return ctx.YarnTypes.Where(p => p.YarnTypeId == YarnTypeId).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<YarnType> GetAllYarnTypes()
+        {
+            try
+            {
+                using (var ctx = new DBAJEntities())
+                {
+                    return ctx.YarnTypes.OrderBy(c => c.YarnTypeName).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AJSoftBAL/YarnTypesBL.cs
@@ -0,0 +1,49 @@
+﻿using AJSoftEntity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJSoftBAL
+{
+    public class YarnTypesBL
+    {
+        #region Get Methods
+        public string GetGridData(GridSettings grid)
+        {
+            try
+            {
+                using (var ctx = new DBAJEntities())
+                {
+                    var query = ctx.YarnTypes.AsQueryable();
+                    int count;
+                    var data = query.GridCommonSettings(grid, out count);
+
+                    var result = new
+                    {
+                        total = (int)Math.Ceiling((double)count / grid.PageSize),
+                        page = grid.PageIndex,
+                        records = count,
+                        rows = (from e in data
+                                select new
+                                {
+                                    e.YarnTypeId,
+                                    e.YarnTypeName,
+                                    e.YarnColorCode,
+                                }).ToArray()
+                    };
+                    return JsonConvert.SerializeObject(result, new IsoDateTimeConverter());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+    }
+}
